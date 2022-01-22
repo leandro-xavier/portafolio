21f64f46a66d5c4b9c1c048a3733c0800ee58d47
Leandro <leaxavier2014@gmail.com>
2022-01-22 02:36:22 -0500

    agregando botones 3d

--- frontend/src/components/Cards/CardWorkComponent.js
+++ frontend/src/components/Cards/CardWorkComponent.js
@@ -1,59 +1,77 @@
 import React from 'react';
+import Card from 'react-animated-3d-card'
 import {Animated} from "react-animated-css";
+import { Button } from '../button/Button';
 
 export const CardWorkComponent = () => {
     const imageAnime = 'img/anime-app-image.png';
     const imageNasa = 'img/nasa-image.png';
     const imagePanawood = 'img/panawood-image.png'
 
-const evento = () => {
-    console.log(evento)
-}
+    const redireccionarPagina = () => {
+        window.setTimeout( abrirURL, 1 ); // 3 segundos
+    };
+    
+    function abrirURL(){
+      //Abrir URL que necesites
+      window.open("https://leandro-xavier.github.io/anime-app-v/")
+      console.log("Han pasado 3 segundos");
+    };
     return (
         <div>
             <div className="container-work">
 
                 <div>
-                <Animated animationIn="flash" animationOut="fadeOut" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
-                <h1 style={{textAlign:"center"}}>Proyectos</h1>
+                    <h1 style={{textAlign:"center"}}>Proyectos</h1>
                     <p style={{textAlign:"center"}}>Algunos de los proyectos </p>
-                </Animated>
                 </div>
 
-                    <div onMouseEnter={evento} className="card-container">
-                            <img src={imageAnime} alt="" className="image animado encima" style={{width: "100%"}}/>
-                                <div className="content-intern">
-                                    <div className="content-hover">
-                                        <h5 className='animate__animated animate__bounce animate__repeat-2'>Anime App</h5>
-                                        <a href="https://leandro-xavier.github.io/anime-app-v/">ver proyecto</a>
-                                    </div>
-                                </div>
-                        </div>
-                 
+                    <div className="card-container" data-aos="fade-down" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000">
+                        <Card       
+                            style={{
+                                backgroundImage: `url(${imageAnime})`,
+                                width: '350px',
+                                 height: '500px',
+                                cursor: 'pointer'
+                            }}
+                            onClick={redireccionarPagina}
+                        />
+                        <Animated animationIn="flash" animationOut="fadeOut" isVisible={true} animationInDuration={Infinity}>
+                            <div className='boton'>
+                                <Button/>
+                            </div>
+                        </Animated>
+                    </div>
+                
+                    <div className="card-container" data-aos="fade-down" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000">
+                        <Card
+
+                                style={{
+                                    backgroundImage: `url(${imagePanawood})`,
+                                    width: '350px',
+                                    height: '500px',
+                                    cursor: 'pointer',
+                                }}
+                                onClick={redireccionarPagina}
+                        />
+                         <Animated animationIn="flash" animationOut="fadeOut" isVisible={true} animationInDuration={Infinity}>
+                            <div className='boton'>
+                                <Button/>
+                            </div>
+                        </Animated>
+                    
+                    </div>
+
+                    <div className="card-container">
+                   
+                    </div>
 
                 <div className="card-container">
-                <Animated animationIn="bounce" animationOut="fadeOut" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
-                <img src={imagePanawood} alt="" className="image animado" style={{width: "100%"}}/>
-                        <div className="content-intern">
-                            <div className="content-hover">
-                                <h5>Panawood</h5>
-                                <a href="https://leandro-xavier.github.io/PanaWood/">ver proyecto</a>
-                            </div>
-                        </div>
-</Animated>
-            
-                </div>
-
-                <div className="card-container">
-                    <img src={imageNasa} alt="" className="image animado" style={{width: "100%"}}/>
-                        <div className="content-intern">
-                            <div className="content-hover">
-                                <h5>Nasa App</h5>
-                                <a href="https://leandro-xavier.github.io/app-de-la-nasa/">ver proyecto</a>
-                            </div>
-                        </div>
+                    
                 </div>
             </div>
+
+            
         </div>
     )
 }
--- frontend/src/components/Navbar/Navigation.js
+++ frontend/src/components/Navbar/Navigation.js
@@ -1,6 +1,8 @@
+
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faBars } from '@fortawesome/free-solid-svg-icons';
+
 
 const Navigation = () => {
   const [click, setClick] = useState(false);
